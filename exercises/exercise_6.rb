b6dfe74b41222baa77284eba6f33f6be31a2b4c4
ethenalee <yeonji900111@gmail.com>
2019-01-15 17:40:47 +0000

    exercise_7 validation set up

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,76 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(
+    first_name: "Khurram", 
+    last_name: "Virani", 
+    hourly_rate: 60
+    )
+
+@store1.employees.create(
+    first_name: "Ethena", 
+    last_name: "Lee", 
+    hourly_rate: 65
+)
+
+@store1.employees.create(
+    first_name: "Yunji", 
+    last_name: "Lee", 
+    hourly_rate: 70
+)
+
+@store1.employees.create(
+    first_name: "Sunghee", 
+    last_name: "lee", 
+    hourly_rate: 100
+)
+
+@store1.employees.create(
+    first_name: "Khurram", 
+    last_name: "Virani", 
+    hourly_rate: 60
+    )
+
+@store1.employees.create(
+    first_name: "Ethena", 
+    last_name: "Lee", 
+    hourly_rate: 65
+)
+
+@store1.employees.create(
+    first_name: "Yunji", 
+    last_name: "Lee", 
+    hourly_rate: 70
+)
+
+@store1.employees.create(
+    first_name: "Sunghee", 
+    last_name: "Lee", 
+    hourly_rate: 100
+)
+
+@store2.employees.create(
+    first_name: "Clementine", 
+    last_name: "Bread", 
+    hourly_rate: 60
+    )
+
+@store2.employees.create(
+    first_name: "Luis", 
+    last_name: "Iteriate", 
+    hourly_rate: 65
+)
+
+@store2.employees.create(
+    first_name: "Sam", 
+    last_name: "John", 
+    hourly_rate: 70
+)
+
+@store2.employees.create(
+    first_name: "Jane", 
+    last_name: "Collin", 
+    hourly_rate: 100
+)
+
+puts Employee.count
